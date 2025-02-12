afa1f38fcebaafa9da727148d6c6ae56eb839961
aydar <sapirovaydar23@gmail.com>
2025-02-13 01:55:38 +0300

    New event handler

--- ISRPO1/ISRPO1/Window1.xaml.cs
+++ ISRPO1/ISRPO1/Window1.xaml.cs
@@ -23,5 +23,10 @@ namespace ISRPO1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Content = "Hi";
+        }
     }
 }
